91439f4c366d1dd6cadb47c8a35b5bb9fa6ea685
muba123321/ <laarychambas@gmail.com>
2024-10-03 02:22:59 -0400

    created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <h1>RealTymSoccer</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Live Scores</li>
+          <li>Fixtures</li>
+          <li>Teams</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
