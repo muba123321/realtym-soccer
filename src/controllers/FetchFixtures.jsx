d8e83bb75f55e37d39604f7c9507ae2767060657
muba123321/ <laarychambas@gmail.com>
2024-10-05 00:53:25 -0400

    optimised api_url for production

--- src/controllers/FetchFixtures.jsx
+++ src/controllers/FetchFixtures.jsx
@@ -1,10 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const apiToken = import.meta.env.VITE_API_TOKEN;
+const apiBaseUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : "/api";
 export const fetchFixtures = createAsyncThunk(
   "soccer/fetchFixtures",
   async (leagueId) => {
-    const response = await axios.get(`/api/v3/football/fixtures`, {
+    const response = await axios.get(`${apiBaseUrl}/v3/football/fixtures`, {
       params: {
         api_token: apiToken,
 
--- src/controllers/FetchLeagues.jsx
+++ src/controllers/FetchLeagues.jsx
@@ -3,10 +3,12 @@ import axios from "axios";
 
 const apiToken = import.meta.env.VITE_API_TOKEN;
 
+const apiBaseUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : "/api";
+
 export const fetchLeagues = createAsyncThunk(
   "soccer/fetchLeagues",
   async (page = 1) => {
-    const response = await axios.get("/api/v3/football/leagues", {
+    const response = await axios.get(`${apiBaseUrl}/v3/football/leagues`, {
       params: {
         api_token: apiToken,
         // "L3Bn8ZB6nHmk65k5ATpOxsbK8Htf2KWzSdZ1od7yXnqYLKWUPWSPw1yE9Osf",
